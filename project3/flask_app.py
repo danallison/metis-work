15a244a6da02988a8be7fa66fa5af3248b6c603c
Dan Allison <daniel.j.allison@gmail.com>
2018-05-01 20:06:23 -0700

    Add sqlalchemy to flask app

--- project3/flask_app.py
+++ project3/flask_app.py
@@ -1,15 +1,20 @@
 from flask import Flask
+from sqlalchemy import create_engine
 
-import numpy as np
-import json
+import os
 
 app = Flask(__name__)
+sql_engine = create_engine('postgresql://{user}:{password}@{host}:{port}/{user}'.format(
+    user=os.environ['POSTGRES_USER'],
+    password=os.environ['POSTGRES_PASSWORD'],
+    host='db', # docker-compose service name
+    port='5432'
+))
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
-
-@app.route('/uniform/<shape>')
-def uniform(shape):
-    shape = tuple(int(s) for s in shape.split('x'))
-    return json.dumps(np.random.uniform(size=shape).tolist())
+    with sql_engine.connect() as connection:
+        id, key, val = connection.execute(
+            'select * from foo limit 1'
+        ).fetchone()
+    return '<h1>Hi, World!</h1><p>{}:{}:{}</p>'.format(id, key, val)
