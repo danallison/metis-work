1f23fb1ae4c1b7a8a1487681c42a48d862ff360c
Dan Allison <daniel.j.allison@gmail.com>
2018-05-05 16:57:09 -0700

    Continue project3 EDA

--- project3/README.md
+++ project3/README.md
@@ -3,6 +3,9 @@
 - Phishing URLs: https://www.phishtank.com/developer_info.php
 - Random words list (used as search terms): https://randomwordgenerator.com/
 - Benign URLs: ?
+- https://en.wikipedia.org/wiki/List_of_most_popular_websites
+- https://moz.com/top500/pages
+- https://aws.amazon.com/alexa-top-sites/
 
 ## Inspiration
 
--- project3/project3utils.py
+++ project3/project3utils.py
@@ -2,6 +2,7 @@ from sshtunnel import SSHTunnelForwarder
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from urllib.parse import urlparse
+from collections import Counter
 
 from cc2ld import second_level_domains_by_country_code as sld_by_cc
 
@@ -35,21 +36,48 @@ def with_remote_sql_session(function):
         finally:
             session.close()
 
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+vowels = 'aeiou'
+consonants = [char for char in alphabet if char not in vowels]
+digits = '0123456789'
+other_chars = '.-'
+all_chars = alphabet + digits + other_chars
+char_label_map = {
+    '.': 'dot',
+    '-': 'hyphen'
+}
+def get_char_count_label(char):
+    return 'char_' + char_label_map.get(char, char)
+
+def get_domain_char_counts(string, key_prefix=''):
+    counter = Counter(string)
+    counts = {(key_prefix + get_char_count_label(c)): counter[c] for c in all_chars}
+    counts[key_prefix + 'vowels'] = sum(counter[c] for c in vowels)
+    counts[key_prefix + 'consonants'] = sum(counter[c] for c in consonants)
+    counts[key_prefix + 'digits'] = sum(counter[c] for c in digits)
+    counts[key_prefix + 'total_chars'] = len(string)
+    return counts
+
 def get_features_from_url(url):
+    url = url.lower()
+    if url[0:7] not in ('http://', 'https:/'):
+        url = 'http://' + url
     url = urlparse(url)
     full_domain = url.netloc
     chunks = full_domain.split('.')
-    dot_count = len(chunks) - 1
     domain_start_index = -2
     if chunks[-2] in sld_by_cc[chunks[-1]]:
         domain_start_index -= 1
     domain = '.'.join(chunks[domain_start_index:])
     subdomain = '.'.join(chunks[:domain_start_index])
-    return {
+    features = get_domain_char_counts(domain, 'domain_')
+    features.update(get_domain_char_counts(subdomain, 'subdomain_'))
+    features.update({
         'full_domain': full_domain,
         'domain': domain,
         'subdomain': subdomain
-    }
+    })
+    return features
 
 assert get_features_from_url('http://www.foo.co.uk')['domain'] == 'foo.co.uk'
 assert get_features_from_url('http://www.foo.com')['domain'] == 'foo.com'
