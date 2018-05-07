12a71d704159a5b1b36263d9706165a008f945bb
Dan Allison <daniel.j.allison@gmail.com>
2018-05-06 20:55:31 -0700

    Separate domain model from path model

--- project3/project3utils.py
+++ project3/project3utils.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlparse
 from collections import Counter
+from scipy.stats import entropy
 
 from cc2ld import second_level_domains_by_country_code as sld_by_cc
 
@@ -21,13 +22,14 @@ def get_char_count_label(char):
 
 def get_char_counts(string, key_prefix=''):
     counter = Counter(string)
-    counts = {(key_prefix + get_char_count_label(c)): counter[c] for c in other_chars}
+    counts = {(key_prefix + get_char_count_label(c)): counter[c] for c in all_chars}
     counts[key_prefix + 'vowels'] = sum(counter[c] for c in vowels)
     counts[key_prefix + 'consonants'] = sum(counter[c] for c in consonants)
     counts[key_prefix + 'digits'] = sum(counter[c] for c in digits)
     total_chars = len(string)
     counts[key_prefix + 'total_chars'] = total_chars
     counts[key_prefix + 'empty_string'] = int(total_chars == 0)
+    counts[key_prefix + 'entropy'] = entropy(list(counter.values()))
     return counts
 
 def get_features_from_url(url):
@@ -40,14 +42,18 @@ def get_features_from_url(url):
         domain_start_index -= 1
     domain = '.'.join(chunks[domain_start_index:])
     subdomain = '.'.join(chunks[:domain_start_index])
+    path = url.path
+    if path and path[0] == '/': path = path[1:]
+    if path and path[-1] == '/': path = path[:-1]
     features = get_char_counts(domain, 'domain_')
     features.update(get_char_counts(subdomain, 'subdomain_'))
-    features.update(get_char_counts(url.path, 'path_'))
+    features.update(get_char_counts(path, 'path_'))
     features.update({
         'full_domain': full_domain,
         'domain': domain,
         'subdomain': subdomain,
-        'path': url.path,
+        'path': path,
+        'domain_is_dot_com': int(chunks[-1] == 'com'),
         'subdomain_is_www': int(subdomain == 'www'),
         'https': int(url.scheme == 'https'),
         'domain_is_raw_ip': int(len(domain) == (features['domain_digits'] + features['domain_char_dot']))
@@ -58,10 +64,12 @@ assert get_features_from_url('http://www.foo.co.uk')['domain'] == 'foo.co.uk'
 assert get_features_from_url('http://www.foo.com')['domain'] == 'foo.com'
 
 numeric_feature_columns = []
+nonpath_feature_columns = []
+path_feature_columns = []
 for key, val in get_features_from_url('http://www.blah.com/foo').items():
     if type(val) != str:
         numeric_feature_columns.append(key)
-
-def get_numeric_features_list_from_url(url):
-    features = get_features_from_url(url)
-    return [features[column] for column in numeric_feature_columns]
+        if key[0:4] == 'path':
+            path_feature_columns.append(key)
+        else:
+            nonpath_feature_columns.append(key)
